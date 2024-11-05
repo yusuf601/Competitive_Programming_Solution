a383afe73725c504102f582c487b38b26e917a58
yusuf601 <yusufmuhyusuh@gmail.com>
2024-11-05 20:45:14 +0700

    feat:updated solution

--- karl_correlation.cpp
+++ karl_correlation.cpp
@@ -16,6 +16,9 @@ struct DataStatistic{
     int sumX;
     int sumY;
     int size;
+    float XY;
+    float SX;
+    float SY;
 };
 void XSquared(std::array<int,10> &PhyScore,DataStatistic &stat){
     int Squared;
@@ -26,6 +29,8 @@ void XSquared(std::array<int,10> &PhyScore,DataStatistic &stat){
     }
     stat.squareX = sum;
     std::cout << "X square: " << stat.squareX << std::endl;
+    // stat.SX = sqrt(stat.squareX);
+    // std::cout << "Hasil SX: " << stat.SX << std::endl;
 }
 void YSquared(std::array<int,10> &HistoScore,DataStatistic &stat){
     int Squared;
@@ -35,21 +40,28 @@ void YSquared(std::array<int,10> &HistoScore,DataStatistic &stat){
         sum += Squared;
     }
     stat.squareY = sum;
-    std::cout << "X Square: " << stat.squareY << std::endl;
+    std::cout << "Y Square: " << stat.squareY << std::endl;
+    // stat.SY = sqrt(stat.squareY);
+    // std::cout << "hasil SY: " << stat.SY << std::endl;
 }
 void FinalDeviation(std::array<int,10> &PhyScore,DataStatistic &stat,std::array<int,10> &HistoScore){
     int sum = 0;
     int RCorrelation;
     int sumY = 0;
     int sumdevisiasi = 0;
+    float SquareDeviX;
+    float SquareDeviY;
+    int SumDevisiasiX = 0;
+    int SumDevisiasiY = 0;
     int ResultMeanY,ResultDeviationY;
-    int ResultMean,ResultDeviationX,SumDeviation;
+    int ResultDeviationX,SumDeviation;
+    int ResultMean;
     // rata-rata x
     for(int i = 0; i <= PhyScore.size() - 1; i++){
         sum += PhyScore[i];
     }
     std::cout << "SUMX: " << sum << std::endl;
-    ResultMean = sum / PhyScore.size(); //devisiasiX
+    ResultMean = (static_cast<float> (sum) / stat.size); //devisiasiX
     std::cout << "MeanX: " << ResultMean << std::endl;
     //rata rata Y
     for(int i = 0; i <= HistoScore.size() - 1; i++){
@@ -61,23 +73,37 @@ void FinalDeviation(std::array<int,10> &PhyScore,DataStatistic &stat,std::array<
     // ini adalah sum X x Y
     for(int i = 0; i <= PhyScore.size() - 1; i++){ // 
         ResultDeviationY = HistoScore[i] - ResultMeanY;
+        SquareDeviY = pow(HistoScore[i] - ResultMeanY,2);
         ResultDeviationX = PhyScore[i] - ResultMean;
-        SumDeviation =  ResultDeviationX * ResultDeviationY;
+        SquareDeviX = pow(static_cast<float>(PhyScore[i]) - ResultMean,2);
+        SumDevisiasiY += SquareDeviY;
+        SumDevisiasiX += SquareDeviX;
+        SumDeviation = ResultDeviationX * ResultDeviationY;
         sumdevisiasi += SumDeviation;
     }
+    stat.SX = SumDevisiasiX;
+    std::cout << "Hasil SX: " << stat.SX << std::endl; 
+    stat.SY = SumDevisiasiY;
+    std::cout << "Hasil SY: " << stat.SY << std::endl; 
     stat.Devisiasi = sumdevisiasi;
     // stat.Devisiasi = sumDeviation;
-    std::cout << "HasilDevisiasi: " << stat.Devisiasi << std::endl;   
+    std::cout << "HasilDevisiasi: " << stat.Devisiasi << std::endl; 
     
 }
 
 void karl_correlation(DataStatistic &stat){
+    float XY;
+    XY = static_cast<float> (stat.Devisiasi) / stat.size;
+    stat.XY = XY;
+    std::cout << "Hasil XY: " << stat.XY << std::endl;
     float r;
-    r = stat.Devisiasi / sqrt(stat.squareX * stat.squareY);
+    r = stat.Devisiasi / sqrt((stat.squareX * stat.squareY));
     std::cout << "Hasil Devisiasi: " << r << std::endl;
 }
 
+void pearson_cor(DataStatistic &stat){
 
+}
 int main(){
     DataStatistic stat;
     std::array<int, 10> PhyScore= {15 ,12,8,8,7,7,7,6 ,5,3};
---
+++ merge_sorted_array.cpp
@@ -0,0 +1,13 @@
+// sumber: https://leetcode.com/problems/merge-sorted-array/description/?envType=problem-list-v2&envId=sorting
+#include <iostream>
+#include <vector>
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+        
+}
+
+int main(){
+    std::vector<int> nums1;
+    std::vector<int> nums2;
+    nums1.po
+    int m,n;
+}
